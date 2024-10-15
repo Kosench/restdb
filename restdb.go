b8f71632bca1e471a0a87e705bec468e12283a4e
Kosench <kosenko.id@dvfu.ru>
2024-10-15 21:59:57 +1000

    Add func ConnectPostgres

--- restdb.go
+++ restdb.go
@@ -1,8 +1,11 @@
 package restdb
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 )
 
 type User struct {
@@ -31,4 +34,17 @@ func (p *User) FromJSON(r io.Reader) error {
 func (p *User) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
+}
+
+func ConnectPostgres() *sql.DB {
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		Hostname, Port, Username, Password, Database)
+
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return db
 }
