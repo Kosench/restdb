517d9e382d4283562af7033a9ce95093e4146379
Kosench <kosenko.id@dvfu.ru>
2024-10-15 22:39:40 +1000

    Add func FindUserID

--- restdb.go
+++ restdb.go
@@ -100,6 +100,40 @@ func DeleteUser(ID int) bool {
 	return true
 }
 
+func FindUserID(ID int) User {
+	db := ConnectPostgres()
+	if db == nil {
+		fmt.Println("Cannot connect to PostgreSQL!")
+		db.Close()
+		return User{}
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users where ID = $1\n", ID)
+	if err != nil {
+		log.Println("Query:", err)
+		return User{}
+	}
+	defer rows.Close()
+
+	u := User{}
+	var c1 int
+	var c2, c3 string
+	var c4 int64
+	var c5, c6 int
+
+	for rows.Next() {
+		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		if err != nil {
+			log.Println(err)
+			return User{}
+		}
+		u = User{c1, c2, c3, c4, c5, c6}
+		log.Println("Found user:", u)
+	}
+	return u
+}
+
 func UserValid(u User) bool {
 	db := ConnectPostgres()
 	if db == nil {
