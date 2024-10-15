7c053d4944a0a3a69c710fecfdcb8e469906a864
Kosench <kosenko.id@dvfu.ru>
2024-10-15 22:48:45 +1000

    Add func ListAllUsers

--- restdb.go
+++ restdb.go
@@ -100,6 +100,37 @@ func DeleteUser(ID int) bool {
 	return true
 }
 
+func ListAllUsers() []User {
+	db := ConnectPostgres()
+	if db == nil {
+		fmt.Println("Cannot connect to PostgreSQL!")
+		db.Close()
+		return []User{}
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users \n")
+	if err != nil {
+		log.Println(err)
+		return []User{}
+	}
+
+	all := []User{}
+	var c1 int
+	var c2, c3 string
+	var c4 int64
+	var c5, c6 int
+
+	for rows.Next() {
+		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		temp := User{c1, c2, c3, c4, c5, c6}
+		all = append(all, temp)
+	}
+
+	log.Println("All:", all)
+	return all
+}
+
 func FindUserID(ID int) User {
 	db := ConnectPostgres()
 	if db == nil {
