eda416a496b238a14e790c60a93750ba5fb4f166
Kosench <kosenko.id@dvfu.ru>
2024-10-15 22:40:12 +1000

    Add func FindUserUsername

--- restdb.go
+++ restdb.go
@@ -134,6 +134,40 @@ func FindUserID(ID int) User {
 	return u
 }
 
+func FindUserUsername(username string) User {
+	db := ConnectPostgres()
+	if db == nil {
+		fmt.Println("Cannot connect to PostgreSQL!")
+		db.Close()
+		return User{}
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users where Username = $1 \n", username)
+	if err != nil {
+		log.Println("FindUserUsername Query:", err)
+		return User{}
+	}
+	defer rows.Close()
+
+	u := User{}
+	var c1 int
+	var c2, c3 string
+	var c4 int64
+	var c5, c6 int
+
+	for rows.Next() {
+		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		if err != nil {
+			log.Println(err)
+			return User{}
+		}
+		u = User{c1, c2, c3, c4, c5, c6}
+		log.Println("Found user:", u)
+	}
+	return u
+}
+
 func UserValid(u User) bool {
 	db := ConnectPostgres()
 	if db == nil {
